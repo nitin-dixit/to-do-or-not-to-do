770bf1743e216ff485dc471691e72bd2c1240069
Nitin Dixit <nitinajaydixit@gmail.com>
2022-04-13 22:17:55 +0530

    remove bg-dark

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -12,7 +12,7 @@ const AppHeader = () => {
           className="rounded-lg list-none min-w-max mt-1 m-0 border-none text-base z-50 text-left float-left py-2 bg-white shadow-lg w-40 dark:text-slate-300 dark:bg-slate-800 dark:hover:bg-slate-700 dark:focus:bg-slate-700 dark:active:bg-slate-700 bg-transparent whitespace-nowrap bg-clip-padding"
           id="status"
         >
-          <option className="bg-slate-900">Due</option>
+          <option>Due</option>
           <option>Completed</option>
         </select>
       </div>

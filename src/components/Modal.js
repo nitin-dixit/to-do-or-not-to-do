a6ff8fcab1fa74a0c115220c4d2b6ec7a446c2bf
Nitin Dixit <nitinajaydixit@gmail.com>
2022-04-20 20:37:10 +0530

    fix modal size

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,7 +14,7 @@ export const Modal = ({ type, modalOn, setModalOn, todo }) => {
   const [taskDeadline, setTaskDeadline] = useState(
     new Date().toISOString().substring(0, 10)
   );
-  const [status] = useState('incomplete');
+  const [status] = useState("incomplete");
 
   const dispatch = useDispatch();
 
@@ -66,7 +66,13 @@ export const Modal = ({ type, modalOn, setModalOn, todo }) => {
           todo.taskDeadline !== taskDeadline
         ) {
           dispatch(
-            updateTodo({ ...todo, taskName, taskDescription, taskDeadline, status })
+            updateTodo({
+              ...todo,
+              taskName,
+              taskDescription,
+              taskDeadline,
+              status,
+            })
           );
           toast.success("Successfully updated!");
         } else {
@@ -96,20 +102,21 @@ export const Modal = ({ type, modalOn, setModalOn, todo }) => {
 
   const children = modalOn ? (
     <div
-      className="absolute flex items-center justify-center z-10 inset-0 outline-none border-n overflow-auto"
+      className="fixed z-10 inset-0 overflow-y-auto"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
     >
-      <div className="flex items-end justify-center min-h-screen pb-28 text-center sm:block sm:p-0">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-32 text-center sm:block sm:p-0">
         <div
-          className="fixed overflow-auto inset-0 bg-black/70 bg-opacity-75 transition-opacity"
+          className="fixed inset-0 bg-black/70 bg-opacity-75 transition-opacity"
           aria-hidden="true"
           onClick={() => {
             toggleModal();
           }}
         ></div>
 
+        {/* This element is to trick the browser into centering the modal contents. */}
         <span
           className="hidden sm:inline-block sm:align-middle sm:h-screen"
           aria-hidden="true"
@@ -117,8 +124,8 @@ export const Modal = ({ type, modalOn, setModalOn, todo }) => {
           &#8203;
         </span>
 
-        <div className="relative inline-block align-top bg-gray-50 dark:text-slate-30  rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full outline-none">
-          <div className="bg-gray-50 dark:text-slate-300 dark:bg-gradient-to-b  dark:from-gray-900 dark:to-gray-800 transition-colors duration-200 px-4 py-2 sm:p-6 sm:pb-4 outline-none">
+        <div className="relative inline-block align-bottom bg-gray-50 dark:text-slate-30  rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full outline-none">
+          <div className="bg-gray-50 dark:text-slate-300 dark:bg-gradient-to-b  dark:from-gray-900 dark:to-gray-800 transition-colors duration-200 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
               <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full text-white bg-rose-500 dark:bg-cyan-400 shadow-xl dark:text-black sm:mx-0 sm:h-10 sm:w-10">
                 <FontAwesomeIcon icon={solid("list-check")} />
@@ -127,14 +134,14 @@ export const Modal = ({ type, modalOn, setModalOn, todo }) => {
               <div className="mt-3 text-left sm:mt-0 sm:ml-4 sm:text-left">
                 <div className="mt-2">
                   <form onSubmit={(e) => handleSubmit(e)}>
-                    <div className="mt-2 sm:flex sm:items-center gap-4">
+                    <div className="sm:flex sm:items-center gap-4">
                       <div className="grid grid-cols-1 gap-6">
                         <input
                           type="text"
                           id="taskName"
                           value={taskName}
                           maxLength={80}
-                          size={35}
+                          size={45}
                           required
                           onChange={(e) => {
                             setTaskName(e.target.value);

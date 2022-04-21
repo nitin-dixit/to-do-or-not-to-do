d5aa1d4a9fcb02f32f624499eb412c0ed590165d
Nitin Dixit <nitinajaydixit@gmail.com>
2022-04-21 10:10:20 +0530

    fix due, package.json app name

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,6 @@ import { Modal } from "./Modal";
 import { motion } from "framer-motion";
 import { TaskName } from "./TaskName";
 
-
 const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
   const [checked, setChecked] = useState(false);
@@ -39,82 +38,83 @@ const TodoItem = ({ todo }) => {
     );
   };
 
-  let due = formatDistanceStrict(new Date(todo.taskDeadline), new Date(), {
-    unit: "day",
-  });
+  let due = formatDistanceStrict(
+    new Date(todo.taskDeadline),
+    new Date(),
+    { addSuffix: true },
+    {
+      locale: "en-IN",
+    }
+  );
   if (due === "0 days") {
     due = "today";
   }
   return (
     <>
       <div className="w-full space-y-5 mb-4">
-        
-          <motion.div
-            className="max-w-xl mx-auto px-4 py-2 bg-white dark:bg-slate-800 dark:text-slate-300 rounded-md shadow-lg"
-            layout
-            initial={{
-              y: 46,
-              scale: 0.3,
-              opacity: 0,
-            }}
-            animate={{
-              y: 0,
-              scale: 1,
-              opacity: 1,
-            }}
-            exit={{
-              y: 46,
-              scale: 0.3,
-              opacity: 0,
-            }}
-            transition={{
-              duration: 0.4,
-            }}
-          >
-            <label className="flex items-center">
-              <input
-                className="rounded text-cyan-500 scale-125 focus:ring-cyan-500"
-                type="checkbox"
-                onChange={() => handleChecked()}
-                checked={checked}
-              />
-              <div className="ml-4 flex-1 whitespace-nowrap text-ellipsis overflow-hidden ...">
-                <TaskName
-                  checked={checked}
-                  taskName={todo.taskName}
-                ></TaskName>
+        <motion.div
+          className="max-w-xl mx-auto px-4 py-2 bg-white dark:bg-slate-800 dark:text-slate-300 rounded-md shadow-lg"
+          layout
+          initial={{
+            y: 46,
+            scale: 0.3,
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            scale: 1,
+            opacity: 1,
+          }}
+          exit={{
+            y: 46,
+            scale: 0.3,
+            opacity: 0,
+          }}
+          transition={{
+            duration: 0.4,
+          }}
+        >
+          <label className="flex items-center">
+            <input
+              className="rounded text-cyan-500 scale-125 focus:ring-cyan-500"
+              type="checkbox"
+              onChange={() => handleChecked()}
+              checked={checked}
+            />
+            <div className="ml-4 flex-1 whitespace-nowrap text-ellipsis overflow-hidden ...">
+              <TaskName checked={checked} taskName={todo.taskName}></TaskName>
 
-                <span className="relative px-1 block font-light text-xs truncate ...">
-                  {todo.taskDescription}
+              <span className="relative px-1 block font-light text-xs truncate ...">
+                {todo.taskDescription}
+              </span>
+              <span className="relative px-1 block text-xs">
+                Due:{" "}
+                <span className="text-red-500 dark:text-red-400 font-normal">
+                  {due}
                 </span>
-                <span className="relative px-1 block text-xs">
-                  <span className="text-red-500 dark:text-red-400 font-normal">
-                    {due === "today" ? `Due ${due}.` : `Due in ${due}.`}
-                  </span>
-                </span>
-                <span className="relative px-1 block font-light text-xs">
-                  created at:{" "}
-                  {format(new Date(todo.creationTime), "p, dd/MM/yyyy")}
-                </span>
-              </div>
+              </span>
+              <span className="relative px-1 block font-light text-xs">
+                created at:{" "}
+                {format(new Date(todo.creationTime), "p, dd/MM/yyyy")}
+              </span>
+            </div>
 
-              <button
-                className="ml-2 focus-visible:outline-cyan-500 hover:text-red-500 mr-2"
-                aria-label="Remove Task"
-                onClick={() => handleDelete()}
-              >
-                <FontAwesomeIcon icon={solid("trash-can")}></FontAwesomeIcon>
-              </button>
-              <button
-                className="ml-2 focus-visible:outline-cyan-500 hover:text-blue-500"
-                aria-label="Edit Task"
-                onClick={() => handleEdit()}
-              >
-                <FontAwesomeIcon icon={solid("pencil")}></FontAwesomeIcon>
-              </button>
-            </label>
-          </motion.div>
-        
+            <button
+              className="ml-2 focus-visible:outline-cyan-500 hover:text-red-500 mr-2"
+              aria-label="Remove Task"
+              onClick={() => handleDelete()}
+            >
+              <FontAwesomeIcon icon={solid("trash-can")}></FontAwesomeIcon>
+            </button>
+            <button
+              className="ml-2 focus-visible:outline-cyan-500 hover:text-blue-500"
+              aria-label="Edit Task"
+              onClick={() => handleEdit()}
+            >
+              <FontAwesomeIcon icon={solid("pencil")}></FontAwesomeIcon>
+            </button>
+          </label>
+        </motion.div>
       </div>
       <Modal
         modalOn={updateModalOn}
